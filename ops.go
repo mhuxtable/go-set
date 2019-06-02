395234b1cf4248488ca8d93768f07bbdd91be221
Matthew Huxtable <matthew.huxtable@cantab.net>
2019-06-02 19:43:02 +0100

    Add docs for new operations

--- ops.go
+++ ops.go
@@ -1,9 +1,11 @@
 package set
 
+// Union returns the set union of sets s1 and s2 by mutating s1
 func (s1 *set) Union(s2 Set) {
 	s1.Add(s2.Slice()...)
 }
 
+// Subtract subtracts set s2 from set s1, mutating the set s1 in-place.
 func (s1 *set) Subtract(s2 Set) {
 	if s1 == nil {
 		return
