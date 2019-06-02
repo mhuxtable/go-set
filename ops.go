d31754c7cd6dbd5c7f6bff39e36b6157056e35e5
Matthew Huxtable <matthew.huxtable@cantab.net>
2019-06-02 20:09:51 +0100

    Non-mutating Union and Subtract operations
    Introduce operations at the package-level which return a new set when
    performing set theoretic operations, rather than mutating one of the
    input sets.

--- ops.go
+++ ops.go
@@ -17,3 +17,32 @@ func (s1 *set) Subtract(s2 Set) {
 		}
 	}
 }
+
+// Union returns the set union of sets s1 and s2 in a new Set, without mutating
+// the input Sets.
+func Union(s1, s2 Set) Set {
+	if s1.Count() == 0 && s2.Count() == 0 {
+		return Set{}
+	}
+
+	var s Set
+	s.Add(s1.Slice()...)
+	s.Add(s2.Slice()...)
+	return s
+}
+
+// Subtract returns the subtraction of set s2 from set s1 in a new Set, without
+// mutating the input Sets.
+func Subtract(s1, s2 Set) Set {
+	if s1.Count() == 0 {
+		return Set{}
+	}
+	if s2.Count() == 0 {
+		return s1
+	}
+
+	var s Set
+	s.Add(s1.Slice()...)
+	s.Subtract(s2)
+	return s
+}
--- ops_test.go
+++ ops_test.go
@@ -6,6 +6,67 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+type testCase struct {
+	leftElements, rightElements, expect []interface{}
+}
+
+var (
+	unionTestCases = []testCase{{
+		leftElements:  []interface{}{},
+		rightElements: []interface{}{},
+		expect:        []interface{}{},
+	}, {
+		leftElements:  []interface{}{},
+		rightElements: []interface{}{3, 4, 5},
+		expect:        []interface{}{3, 4, 5},
+	}, {
+		leftElements:  []interface{}{1, 2, 3},
+		rightElements: []interface{}{},
+		expect:        []interface{}{1, 2, 3},
+	}, {
+		leftElements:  []interface{}{1, 2, 3, 4, 5},
+		rightElements: []interface{}{1, 2, 3, 4, 5},
+		expect:        []interface{}{1, 2, 3, 4, 5},
+	}, {
+		leftElements:  []interface{}{1, 2, 3, 4, 5},
+		rightElements: []interface{}{6, 7, 8, 9},
+		expect:        []interface{}{1, 2, 3, 4, 5, 6, 7, 8, 9},
+	}, {
+		leftElements:  []interface{}{1, 2, 3, 4, 5},
+		rightElements: []interface{}{4, 5, 6, 7, 8},
+		expect:        []interface{}{1, 2, 3, 4, 5, 6, 7, 8},
+	}}
+
+	subtractTestCases = []testCase{{
+		leftElements:  []interface{}{},
+		rightElements: []interface{}{},
+		expect:        []interface{}{},
+	}, {
+		leftElements:  []interface{}{1, 2, 3},
+		rightElements: []interface{}{},
+		expect:        []interface{}{1, 2, 3},
+	}, {
+		leftElements:  []interface{}{1, 2, 3, 4, 5},
+		rightElements: []interface{}{6, 7, 8, 9},
+		expect:        []interface{}{1, 2, 3, 4, 5},
+	}, {
+		leftElements:  []interface{}{1, 2, 3, 4, 5},
+		rightElements: []interface{}{1, 2, 3, 4, 5},
+		expect:        []interface{}{},
+	}, {
+		leftElements:  []interface{}{1, 2, 3, 4, 5},
+		rightElements: []interface{}{5, 4, 3, 7, 8, 9},
+		expect:        []interface{}{1, 2},
+	}}
+)
+
+func makeSet(t *testing.T, xs []interface{}) Set {
+	var s Set
+	s.Add(xs...)
+	require.Equal(t, len(xs), s.Count())
+	return s
+}
+
 func TestSetUnion(t *testing.T) {
 	t.Run("Union of two uninitialised Sets is a no-op", func(t *testing.T) {
 		var s1, s2 Set
@@ -25,30 +86,10 @@ func TestSetUnion(t *testing.T) {
 	})
 
 	t.Run("Union of various initialised Sets returns the expected result", func(t *testing.T) {
-		tcs := []struct {
-			leftElements, rightElements, expect []interface{}
-		}{{
-			leftElements:  []interface{}{1, 2, 3, 4, 5},
-			rightElements: []interface{}{1, 2, 3, 4, 5},
-			expect:        []interface{}{1, 2, 3, 4, 5},
-		}, {
-			leftElements:  []interface{}{1, 2, 3, 4, 5},
-			rightElements: []interface{}{6, 7, 8, 9},
-			expect:        []interface{}{1, 2, 3, 4, 5, 6, 7, 8, 9},
-		}, {
-			leftElements:  []interface{}{1, 2, 3, 4, 5},
-			rightElements: []interface{}{4, 5, 6, 7, 8},
-			expect:        []interface{}{1, 2, 3, 4, 5, 6, 7, 8},
-		}}
-
-		for _, tc := range tcs {
+		for _, tc := range unionTestCases {
 			var s1, s2 Set
-
-			s1.Add(tc.leftElements...)
-			require.Equal(t, len(tc.leftElements), s1.Count())
-
-			s2.Add(tc.rightElements...)
-			require.Equal(t, len(tc.rightElements), s2.Count())
+			s1 = makeSet(t, tc.leftElements)
+			s2 = makeSet(t, tc.rightElements)
 
 			s1.Union(s2)
 
@@ -70,23 +111,7 @@ func TestSetSubtract(t *testing.T) {
 	})
 
 	t.Run("subtracting two sets produces the expected result", func(t *testing.T) {
-		tcs := []struct {
-			leftElements, rightElements, expect []interface{}
-		}{{
-			leftElements:  []interface{}{1, 2, 3, 4, 5},
-			rightElements: []interface{}{6, 7, 8, 9},
-			expect:        []interface{}{1, 2, 3, 4, 5},
-		}, {
-			leftElements:  []interface{}{1, 2, 3, 4, 5},
-			rightElements: []interface{}{1, 2, 3, 4, 5},
-			expect:        []interface{}{},
-		}, {
-			leftElements:  []interface{}{1, 2, 3, 4, 5},
-			rightElements: []interface{}{5, 4, 3, 7, 8, 9},
-			expect:        []interface{}{1, 2},
-		}}
-
-		for _, tc := range tcs {
+		for _, tc := range subtractTestCases {
 			var s1, s2 Set
 			s1.Add(tc.leftElements...)
 			s2.Add(tc.rightElements...)
@@ -100,4 +125,42 @@ func TestSetSubtract(t *testing.T) {
 			require.ElementsMatch(t, tc.expect, s1.Slice())
 		}
 	})
+}
+
+func TestUnion(t *testing.T) {
+	t.Run("union of two sets returns the expected result", func(t *testing.T) {
+		for _, tc := range unionTestCases {
+			s1 := makeSet(t, tc.leftElements)
+			s2 := makeSet(t, tc.rightElements)
+
+			out := Union(s1, s2)
+
+			// The input sets are not modified
+			require.Equal(t, len(tc.leftElements), s1.Count())
+			require.ElementsMatch(t, tc.leftElements, s1.Slice())
+			require.Equal(t, len(tc.rightElements), s2.Count())
+			require.ElementsMatch(t, tc.rightElements, s2.Slice())
+
+			require.Equal(t, len(tc.expect), out.Count())
+			require.ElementsMatch(t, tc.expect, out.Slice())
+		}
+	})
+}
+
+func TestSubtract(t *testing.T) {
+	for _, tc := range subtractTestCases {
+		s1 := makeSet(t, tc.leftElements)
+		s2 := makeSet(t, tc.rightElements)
+
+		out := Subtract(s1, s2)
+
+		// The input sets are not modified
+		require.Equal(t, len(tc.leftElements), s1.Count())
+		require.ElementsMatch(t, tc.leftElements, s1.Slice())
+		require.Equal(t, len(tc.rightElements), s2.Count())
+		require.ElementsMatch(t, tc.rightElements, s2.Slice())
+
+		require.Equal(t, len(tc.expect), out.Count())
+		require.ElementsMatch(t, tc.expect, out.Slice())
+	}
 }
