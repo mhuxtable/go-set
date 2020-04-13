e76e299e62e9d241c1cb74db6f283523051bc1e2
Matthew Huxtable <matthew.huxtable@cantab.net>
2020-04-14 00:25:48 +0100

    sort order

--- cmd/genset/templates/generate.sh
+++ cmd/genset/templates/generate.sh
@@ -13,7 +13,7 @@ var (
 EOF
 
 TEMPLATES=
-for x in $(ls $DIR/*.tpl); do
+for x in $(ls $DIR/*.tpl | sort); do
 	# Not tolerant to non-ASCII template filenames. Okay for now...
 	tpl="$(<"$x")"
 	tpl=${tpl//\`/"\` + \"\`\" + \`"}
